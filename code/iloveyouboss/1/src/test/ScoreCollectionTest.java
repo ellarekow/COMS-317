202758c6c4b4f963326993eff5bc4c3b670f1344
Ella Rekow <ellarekow@gmail.com>
2023-01-23 12:15:07 -0600

    add exsisting project files to git

---
+++ code/iloveyouboss/1/src/test/ScoreCollectionTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScoreCollectionTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void arithmeticMean() {
+    }
+}
